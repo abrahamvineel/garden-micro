c80742d793ca0b1cdf5cd7258f02d6e02c359fa7
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-06-30 14:19:25 -0400

    inventory service

---
+++ inventory-service/src/main/java/com/garden/inventory/inventory_service/controller/InventoryController.java
@@ -0,0 +1,20 @@
+package com.garden.inventory.inventory_service.controller;
+
+import com.garden.inventory.inventory_service.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/inventory")
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping("/{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+        return inventoryService.isInStock(skuCode);
+    }
+}
