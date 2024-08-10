bd6a510534728621266dc1b20b06a3b2edf602c6
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-08-10 19:54:39 -0400

    remove unused import

--- product-service/src/main/java/com/garden/product/product_service/model/Product.java
+++ product-service/src/main/java/com/garden/product/product_service/model/Product.java
@@ -1,8 +1,6 @@
 package com.garden.product.product_service.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
